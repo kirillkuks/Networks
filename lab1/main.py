ebdedb66b8187cd66e9bf22e15cbec4e851ba78a
Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2023-10-31 22:26:05 +0300

    finish lab code and update report

--- lab1/main.py
+++ lab1/main.py
@@ -1,19 +1,36 @@
+from __future__ import annotations
 from threading import Thread
+from typing import List
+from matplotlib import pyplot as plt
+from enum import Enum
+from time import time
 import protocol as net
+import numpy as np
+import os
 
 
+kMessagesToSend = 100
+kThisFilePath = os.path.abspath(__file__)
 
-def main():
-    #sender = net.GoBackNSender(20, 0.5)
-    #receiver = net.GoBackNReceiver(0.5)
+def img_save_dst() -> str:
+    return 'doc\\img\\'
 
-    sender = net.SelectiveRepeatSender(20, 0.5)
-    receiver = net.SelectiveReapetReceiver(0.3)
 
-    sender.set_max_messages_num(100)
+class Statistics(Enum):
+    kMessageNum = 0,
+    kWorkingTime = 1
 
-    net.SlidingWindowProtocol.connect(sender, receiver)
+    @staticmethod
+    def to_str(stat: Statistics) -> str:
+        if stat == Statistics.kMessageNum:
+            return 'MessageNum'
+        elif stat == Statistics.kWorkingTime:
+            return 'WorkingTime'
+        
+        return ''
 
+
+def run_protocol(sender: net.SlidingWindowSender, receiver: net.SlidingWindowReceiver) -> None:
     sender_thread = Thread(target=sender.run)
     receiver_thread = Thread(target=receiver.run)
 
@@ -22,6 +39,166 @@ def main():
 
     sender_thread.join()
     receiver_thread.join()
+
+
+def calculate_corruption_rate_dependencies(
+        protocol_type: net.Protocol,
+        window_size: int,
+        timeout: float,
+        corruption_rates: List[float],
+        stat: Statistics,
+        show_plot: bool = True) -> None:
+    messages_nums = []
+    work_times = []
+
+    for corruption_rate in corruption_rates:
+        sender, receiver = net.SlidingWindowProtocol.create_connected(protocol_type, window_size, timeout, corruption_rate)
+        sender.set_max_messages_num(kMessagesToSend)
+
+        start_time = time()
+        run_protocol(sender, receiver)
+        work_time = time() - start_time
+
+        print(f'corruption_rate = {corruption_rate}, message_num = {sender.get_sended_messages_num()}, work_time = {work_time}')
+        messages_nums.append(sender.get_sended_messages_num())
+        work_times.append(work_time)
+
+    plt.plot(corruption_rates, messages_nums if stat == Statistics.kMessageNum else work_times, label=f'window size = {window_size}')
+    if show_plot:
+        plt.legend()
+        plt.show()
+
+
+def calculate_window_size_dependencies(
+        protocol_type: net.Protocol,
+        timeout: float,
+        corruption_rate: float,
+        window_sizes: List[int],
+        stat: Statistics,
+        show_plot: bool = True) -> None:
+    messages_nums = []
+    work_times = []
+
+    for window_size in window_sizes:
+        sender, receiver = net.SlidingWindowProtocol.create_connected(protocol_type, window_size, timeout, corruption_rate)
+        sender.set_max_messages_num(kMessagesToSend)
+
+        start_time = time()
+        run_protocol(sender, receiver)
+        work_time = time() - start_time
+
+        print(f'window_size = {window_size}, message_num = {sender.get_sended_messages_num()}, work_time = {work_time}')
+        messages_nums.append(sender.get_sended_messages_num())
+        work_times.append(work_time)
+
+    plt.plot(window_sizes, messages_nums if stat == Statistics.kMessageNum else work_times, label=f'corruption rate = {corruption_rate}')
+    if show_plot:
+        plt.legend()
+        plt.show()
+
+
+def calculate_timeout_dependencies(
+        protocol_type: net.Protocol,
+        window_size: int,
+        corruption_rate: float,
+        timeouts: List[float],
+        stat: Statistics,
+        show_plot: bool = True) -> None:
+    messages_nums = []
+    work_times = []
+
+    for timeout in timeouts:
+        sender, receiver = net.SlidingWindowProtocol.create_connected(protocol_type, window_size, timeout, corruption_rate)
+        sender.set_max_messages_num(kMessagesToSend)
+
+        start_time = time()
+        run_protocol(sender, receiver)
+        work_time = time() - start_time
+
+        print(f'window_size = {window_size}, message_num = {sender.get_sended_messages_num()}, work_time = {work_time}')
+        messages_nums.append(sender.get_sended_messages_num())
+        work_times.append(work_time)
+
+    plt.plot(timeouts, messages_nums if stat == Statistics.kMessageNum else work_times, label=net.Protocol.to_str(protocol_type))
+    if show_plot:
+        plt.legend()
+        plt.show()
+
+
+
+def calculate_size_rate_dependencies(
+        protocol_type: net.Protocol,
+        timeout: float,
+        window_sizes: List[int],
+        corruption_rates: List[float],
+        stat: Statistics) -> None:
+    
+    for window_size in window_sizes:
+        calculate_corruption_rate_dependencies(protocol_type, window_size, timeout, corruption_rates, stat, False)
+
+    plt.legend()
+    plt.xlabel('corruption rate')
+    plt.ylabel('total messages send' if stat == Statistics.kMessageNum else 'working time (in seconds)')
+    plt.title(f'{net.Protocol.to_str(protocol_type)}')
+    plt.savefig(f'{img_save_dst()}sizeRate{net.Protocol.to_short_str(protocol_type)}{Statistics.to_str(stat)}.png')
+    plt.clf()
+
+
+def calculate_rate_size_dependencies(
+        protocol_type: net.Protocol,
+        timeout: float,
+        corruption_rates: List[float],
+        window_sizes: List[int],
+        stat: Statistics) -> None:
+    
+    for corruption_rate in corruption_rates:
+        calculate_window_size_dependencies(protocol_type, timeout, corruption_rate, window_sizes, stat, False)
+
+    plt.legend()
+    plt.xlabel('window size')
+    plt.ylabel('total messages send' if stat == Statistics.kMessageNum else 'working time (in seconds)')
+    plt.title(f'{net.Protocol.to_str(protocol_type)}')
+    plt.savefig(f'{img_save_dst()}rateSize{net.Protocol.to_short_str(protocol_type)}{Statistics.to_str(stat)}.png')
+    plt.clf()
+
+
+def calculate_protocol_timeout_dependencies(
+        window_size: int,
+        corruption_rate: float,
+        protocol_types: List[net.Protocol],
+        timeouts: List[float],
+        stat: Statistics) -> None:
+    
+    for protocol_type in protocol_types:
+        calculate_timeout_dependencies(protocol_type, window_size, corruption_rate, timeouts, stat, False)
+
+    plt.legend()
+    plt.xlabel('timeout (in seconds)')
+    plt.ylabel('total messages send' if stat == Statistics.kMessageNum else 'working time (in seconds)')
+    plt.title('')
+    plt.savefig(f'{img_save_dst()}timeouts{Statistics.to_str(stat)}.png')
+    plt.clf()
+
+
+def main():
+    corruption_rates = np.linspace(0.0, 0.9, 19)
+    window_sizes = [int(x) for x in np.linspace(5, 50, 10)]
+    timeouts = np.linspace(0.02, 0.5, 30)
+
+    calculate_size_rate_dependencies(net.Protocol.kSrp, 0.5, [10, 25, 50], corruption_rates, Statistics.kMessageNum)
+    calculate_rate_size_dependencies(net.Protocol.kSrp, 0.5, [0.1, 0.25, 0.5], window_sizes, Statistics.kMessageNum)
+
+    calculate_size_rate_dependencies(net.Protocol.kGbn, 0.5, [10, 25, 50], corruption_rates, Statistics.kMessageNum)
+    calculate_rate_size_dependencies(net.Protocol.kGbn, 0.5, [0.1, 0.25, 0.5], window_sizes, Statistics.kMessageNum)
+
+    calculate_size_rate_dependencies(net.Protocol.kSrp, 0.5, [10, 25, 50], corruption_rates, Statistics.kWorkingTime)
+    calculate_rate_size_dependencies(net.Protocol.kSrp, 0.5, [0.1, 0.25, 0.5], window_sizes, Statistics.kWorkingTime)
+
+    calculate_size_rate_dependencies(net.Protocol.kGbn, 0.5, [10, 25, 50], corruption_rates, Statistics.kWorkingTime)
+    calculate_rate_size_dependencies(net.Protocol.kGbn, 0.5, [0.1, 0.25, 0.5], window_sizes, Statistics.kWorkingTime)
+
+    calculate_protocol_timeout_dependencies(10, 0.0, [net.Protocol.kGbn, net.Protocol.kSrp], timeouts, Statistics.kMessageNum)
+    calculate_protocol_timeout_dependencies(10, 0.0, [net.Protocol.kGbn, net.Protocol.kSrp], timeouts, Statistics.kWorkingTime)
 
     return
 
--- lab1/protocol.py
+++ lab1/protocol.py
@@ -1,11 +1,36 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from numpy import random as rnd
-from datetime import datetime
-from time import sleep
+from time import time, sleep
 
+
+def get_current_time() -> float:
+    return time()
+
+
+class Protocol(Enum):
+    kGbn = 0,
+    kSrp = 1
+
+    @staticmethod
+    def to_str(protocol: Protocol) -> str:
+        if protocol == Protocol.kGbn:
+            return 'Go-Back-N'
+        elif protocol == Protocol.kSrp:
+            return 'Selective Repeat'
+        
+        return ''
+    
+    @staticmethod
+    def to_short_str(protocol: Protocol) -> str:
+        if protocol == Protocol.kGbn:
+            return 'GBN'
+        elif protocol == Protocol.kSrp:
+            return 'SRP'
+        
+        return ''
 
 class MsgCode(Enum):
     kSuccess = 0,
@@ -25,6 +50,25 @@ class SlidingWindowProtocol(ABC):
         sender.connect(receiver)
         receiver.connect(sender)
 
+    def create_connected(
+            type: Protocol,
+            window_size: int,
+            timeout: float,
+            corruption_rate: float) -> Tuple[SlidingWindowSender, SlidingWindowReceiver]:
+        sender: SlidingWindowSender = None
+        receiver: SlidingWindowReceiver = None
+
+        if type == Protocol.kGbn:
+            sender = GoBackNSender(window_size, timeout)
+            receiver = GoBackNReceiver(corruption_rate)
+        elif type == Protocol.kSrp:
+            sender = SelectiveRepeatSender(window_size, timeout)
+            receiver = SelectiveReapetReceiver(corruption_rate)
+        else:
+            assert False
+        
+        SlidingWindowProtocol.connect(sender, receiver)
+        return sender, receiver
 
     def __init__(self) -> None:
         super().__init__()
@@ -62,13 +106,16 @@ class SlidingWindowSender(SlidingWindowProtocol):
 
     def is_finished(self) -> bool:
         return self.finished
+    
+    def get_sended_messages_num(self) -> int:
+        return self.message_counter
 
 
 class SlidingWindowReceiver(SlidingWindowProtocol):
-    def __init__(self, corruption_chance: float) -> None:
+    def __init__(self, corruption_rate: float) -> None:
         super().__init__()
         self.sender: SlidingWindowSender = None
-        self.corruption_chance = max(min(corruption_chance, 1.0), 0.0)
+        self.corruption_rate = max(min(corruption_rate, 1.0), 0.0)
         self.rnd = rnd.default_rng()
 
     def connect(self, sender: SlidingWindowSender) -> None:
@@ -78,7 +125,7 @@ class SlidingWindowReceiver(SlidingWindowProtocol):
     def prepare_answer(self, message_id: int) -> Message:
         corruption_rnd = self.rnd.uniform(0.0, 1.0)
 
-        if corruption_rnd < self.corruption_chance:
+        if corruption_rnd < self.corruption_rate:
             # corrupted
             return Message(message_id, MsgCode.kFail, None)
 
@@ -90,14 +137,14 @@ class GoBackNSender(SlidingWindowSender):
     def __init__(self, window_size: int, timeout_time: float) -> None:
         super().__init__(window_size, timeout_time)
         self.send_base = 0
-        self.send_base_time = datetime.now()
+        self.send_base_time = get_current_time()
         self.send_next = 0
         self.message_id = 0
         self.waiting_message_id = 0
         self.dummy_data = 'go back n sender data'
 
     def run(self) -> None:
-        print('sender start work')
+        #print('sender start work')
 
         while self.send_base < self.max_messages_num:
             handle_error = False
@@ -106,7 +153,7 @@ class GoBackNSender(SlidingWindowSender):
                 send_message = Message(self.message_id, MsgCode.kSuccess, self.dummy_data)
                 self.send_message_to_receiver(send_message)
         
-                self.send_base_time = datetime.now()
+                self.send_base_time = get_current_time()
                 self.send_next += 1
                 self.message_id += 1
                 # print(f'sender send id {send_message.id}')
@@ -118,17 +165,17 @@ class GoBackNSender(SlidingWindowSender):
                     self.send_base += 1
                     self.waiting_message_id += 1
                     del self.message_queue[0]
-                    print(f'sender move window, new base {self.send_base}')
+                    #print(f'sender move window, new base {self.send_base}')
                 else:
                     handle_error = True
 
             if handle_error or self.time_since_base_send() > self.timeout_time:
-                print(f'moved back to {self.send_next - self.send_base}')
+                #print(f'moved back to {self.send_next - self.send_base}')
                 self.send_next = self.send_base
                 self.waiting_message_id = self.message_id
 
         self.finished = True
-        print(f'GBN total sended: {self.message_counter}')
+        #print(f'GBN total sended: {self.message_counter}')
 
     def remove_outdated_messages(self) -> int:
         timouted_messages = 0
@@ -146,16 +193,16 @@ class GoBackNSender(SlidingWindowSender):
         return queue_size - timouted_messages
 
     def time_since_base_send(self) -> float:
-        return (datetime.now() - self.send_base_time).microseconds * 1e-6
-
+        return get_current_time() - self.send_base_time
+    
 
 class GoBackNReceiver(SlidingWindowReceiver):
-    def __init__(self, corruption_chance: float) -> None:
-        super().__init__(corruption_chance)
+    def __init__(self, corruption_rate: float) -> None:
+        super().__init__(corruption_rate)
         self.last_received = 0
 
     def run(self) -> None:
-        print('receiver start work')
+        #print('receiver start work')
 
         while not self.sender.is_finished():
             if len(self.message_queue) > 0:
@@ -170,7 +217,7 @@ class GoBackNReceiver(SlidingWindowReceiver):
 
 class SelectiveRepeatSender(SlidingWindowSender):
     class MessageNode:
-        def __init__(self, send_time: datetime, message: Message) -> None:
+        def __init__(self, send_time: float, message: Message) -> None:
             self.send_time = send_time
             self.message = message
 
@@ -183,55 +230,57 @@ class SelectiveRepeatSender(SlidingWindowSender):
 
     def run(self) -> None:
         while self.finished == False:
-            if len(self.message_nodes) < self.window_size:
-                send_message = Message(self.send_next, MsgCode.kSuccess, self.dummy_data)
-
-                self.send_message_to_receiver(send_message)
-                self.message_nodes[self.send_next] =  SelectiveRepeatSender.MessageNode(datetime.now(), send_message)
-
-                print(f'sender send {self.send_next}')
-                self.send_next += 1
-
             queue_size = len(self.message_queue)
             while queue_size > 0:
                 message = self.message_queue[0]
-                self.last_approved = message.data
-                if self.last_approved >= self.max_messages_num:
+                self.last_approved = max(self.last_approved, message.data)
+
+                if self.last_approved >= self.max_messages_num - 1:
                     self.finished = True
                     break
 
                 if message.id in self.message_nodes:
                     if message.code == MsgCode.kSuccess:
-                        print(f'approve {message.id}')
                         del self.message_nodes[message.id]
                     else:
                         self.send_message_to_receiver(self.message_nodes[message.id].message)
-                        self.message_nodes[message.id].send_time = datetime.now()
-                        print(f'repeat {message.id}')
+                        self.message_nodes[message.id].send_time = get_current_time()
 
                 del self.message_queue[0]
                 queue_size -= 1
 
+            if len(self.message_nodes) < self.window_size and self.send_next < self.max_messages_num:
+                send_message = Message(self.send_next, MsgCode.kSuccess, self.dummy_data)
+
+                self.send_message_to_receiver(send_message)
+                self.message_nodes[self.send_next] =  SelectiveRepeatSender.MessageNode(get_current_time(), send_message)
+
+                self.send_next += 1
+
             for message_node_idx in self.message_nodes:
                 message_node = self.message_nodes[message_node_idx]
+
                 if self.is_outdated(message_node.send_time):
                     self.send_message_to_receiver(message_node.message)
-                    self.message_nodes[message_node.message.id].send_time = datetime.now()
-                    print(f'repeat outdated {message_node.message.id}')
+                    self.message_nodes[message_node.message.id].send_time = get_current_time()
+                    #print(f'repeat outdated {message_node.message.id}')
 
-        print(f'SRP total sended: {self.message_counter}')
+        #print(f'SRP total sended: {self.message_counter}')
 
-    def is_outdated(self, time: datetime) -> bool:
-        return (datetime.now() - time).microseconds * 1e-6 > self.timeout_time
+    def is_outdated(self, send_time: float) -> bool:
+        #print(f'current = {get_current_time()}, send = {send_time}')
+        return get_current_time() - send_time > self.timeout_time
 
 
 class SelectiveReapetReceiver(SlidingWindowReceiver):
-    def __init__(self, corruption_chance: float) -> None:
-        super().__init__(corruption_chance)
+    def __init__(self, corruption_rate: float) -> None:
+        super().__init__(corruption_rate)
         self.last_received = -1
         self.received: List[int] = []
 
     def run(self) -> None:
+        info_send_time = get_current_time()
+
         while not self.sender.is_finished():
             self.resolve_last_received()
 
@@ -247,6 +296,10 @@ class SelectiveReapetReceiver(SlidingWindowReceiver):
 
                 del self.message_queue[0]
 
+            elif get_current_time() - info_send_time > 0.01: # exta safe
+                info_send_time = get_current_time()
+                self.sender.get_message(Message(-1, MsgCode.kSuccess, self.last_received))
+
     def resolve_last_received(self) -> None:
         i = 0
         while i < len(self.received):
@@ -258,6 +311,4 @@ class SelectiveReapetReceiver(SlidingWindowReceiver):
                 self.last_received += 1
 
             i += 1
-
-        print(f'receiver last received {self.last_received}')
     
